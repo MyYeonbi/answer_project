9d2d35557c4a618787c56c83d89a4c3cb8d1953e
MyYeonbi <yeonbi_kim@icloud.com>
2024-09-18 22:50:54 +0900

    -

--- src/main/java/org/project/backend/repository/PostRepository.java
+++ src/main/java/org/project/backend/repository/PostRepository.java
@@ -2,7 +2,13 @@ package org.project.backend.repository;
 
 import org.project.backend.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p WHERE (6371 * acos(cos(radians(:latitude)) * cos(radians(p.latitude)) * cos(radians(p.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(p.latitude)))) < :distance")
+    List<Post> findNearbyPosts(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("distance") double distance);
 }
